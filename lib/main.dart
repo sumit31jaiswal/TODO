ae7ad6fbba9274c0a99523a0ed318569e7bd99e7
sumitkumar <sumit31jaiswal@gmail.com>
2021-03-26 22:00:56 +0530

    hi

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+        ),
+        home: Home());
+  }
+}
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List todos = List();
+  String input = '';
+  bool _isSelected = false;
+  var valuefirst="";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      appBar: AppBar(
+        title: Text("Todo"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text("Add Todolist"),
+                  content: TextFormField(
+                    onChanged: (String value) {
+                      input = value;
+                    },
+                  ),
+                  actions: <Widget>[
+                    FlatButton(
+                        onPressed: () {
+                          setState(() {
+                            todos.add(input);
+                          });
+                        },
+                        child: Text("Add"))
+                  ],
+                );
+              });
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+                child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    margin: EdgeInsets.all(6),
+                    elevation: 4,
+                    child: ListTile(
+                        title: Text(todos[index]),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            setState(() {
+                              todos.removeAt(index);
+                            });
+                          },
+                        ),
+                        leading: Container(
+                            child: Checkbox(
+                                value: valuefirst==index.toString()?true:false,
+                                onChanged: (bool value) {
+                                  setState(() {
+                                    valuefirst = index.toString();
+                                  });
+                                }))
+                    )
+                )
+            );
+          }),
+    );
+  }
+}
